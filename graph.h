f2b6b0dfefdf3d99f84f869eb08304bf69138844
Winwarot <warotwinwin@gmail.com>
2026-05-11 11:41:29 +0700

    add dijkstra and displayPath to graph
    - add MinHeap (internal, used by dijkstra only)
    - add dijkstra() for shortest path by time, fare, or stops
    - add displayPath() to print route summary and details
    - add PathResult struct to graph.h

--- graph.h
+++ graph.h
@@ -3,20 +3,30 @@
 
 #include "station.h"
 
+typedef struct PathResult {
+    int *path;
+    int pathLen;
+    int totalDist10;
+    int totalTime;
+    int totalFare;
+} PathResult;
+
 typedef struct Graph {
     Station stations[MAX_STATIONS];
     Edge *adjList[MAX_STATIONS];
     int stationCount;
 } Graph;
 
-Graph  *createGraph(void);
-int     graphAddStation(Graph *g, const char *code, const char *name, const char *line, int isInterchange);
-void    graphAddEdge(Graph *g, int from, int to, double dist, int time, int fare);
-void    graphAddUndirectedEdge(Graph *g, int a, int b, double dist, int time, int fare);
-void    graphCloseStation(Graph *g, int idx);
-void    graphReopenStation(Graph *g, int idx);
-void    graphRemoveStation(Graph *g, int idx);
-void    graphDisplay(const Graph *g);
-void    freeGraph(Graph *g);
+Graph *createGraph(void);
+int graphAddStation(Graph *g, const char *code, const char *name, const char *line, int isInterchange);
+void graphAddEdge(Graph *g, int from, int to, double dist, int time, int fare);
+void graphAddUndirectedEdge(Graph *g, int a, int b, double dist, int time, int fare);
+void graphCloseStation(Graph *g, int idx);
+void graphReopenStation(Graph *g, int idx);
+void graphRemoveStation(Graph *g, int idx);
+PathResult dijkstra(Graph *g, int src, int dst, const char *mode);
+void displayPath(const Graph *g, const PathResult *pr);
+void graphDisplay(const Graph *g);
+void freeGraph(Graph *g);
 
 #endif
